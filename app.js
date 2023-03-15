2b4a2901afcc0380f618829d4a72e7c761a1dd3c
john <jjmiyao@gmail.com>
2023-03-14 23:41:20 -0700

    Add views, data and CSS

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-app.js
+import express from 'express';
+const PORT = 2000;
+
+import monstarsData from './monstarsData.json' assert { type: 'json' };
+
+import { URL } from 'node:url';
+const __dirname = new URL ('.', import.meta.url).pathname;
+
+const app = express();
+console.log("Hi");
+
+//EJS setup
+app.set('view engine', 'ejs');
+
+//Static files setup
+app.use(express.static('public'));
+
+//Page Routes
+app.get('/', function(request, response) {
+	response.render('home', { monstarsData });
+	response.sendFile('.styles/site.css', { root: __dirname });
+})
+
+app.get('/monstars', function(request, response) {
+	response.render("monstars", { monstarsData });
+});
+
+app.get('/detail', function(request, response) {
+	response.render('detail');
+});
+
+app.use( function(request, response) {
+	response.status(404).render('404', { query: request.url });
+});
+
+app.listen(PORT, function() {
+	console.log("Server is listening at http://localhost:2000");
+});
