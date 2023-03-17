58ea90d48151dad9290a2c0da7d34e1e14cab0fa
john <jjmiyao@gmail.com>
2023-03-16 18:59:29 -0700

    add favicon, minified css, installed sass

--- app.js
+++ app.js
@@ -1,28 +1,40 @@
 import express from 'express';
-const PORT = 2000;
+const PORT = 1986;
 
-import monstarsData from './monstarsData.json' assert { type: 'json' };
+import monstersData from './monstersData.json' assert { type: 'json' };
 
 import { URL } from 'node:url';
 const __dirname = new URL ('.', import.meta.url).pathname;
 
 const app = express();
-console.log("Hi");
+console.log("- - - - LET'S GET STARTED - - - - ");
+
+app.listen(PORT, function() {
+	console.log("Server is listening at http://localhost:" + PORT);
+});
 
 //EJS setup
 app.set('view engine', 'ejs');
 
 //Static files setup
 app.use(express.static('public'));
+app.use(express.static('css'));
 
 //Page Routes
 app.get('/', function(request, response) {
-	response.render('home', { monstarsData });
-	response.sendFile('.styles/site.css', { root: __dirname });
+	response.render('home', { monsters: monstersData });
 })
 
-app.get('/monstars', function(request, response) {
-	response.render("monstars", { monstarsData });
+app.get('/monsters', function(request, response) {
+	response.render("monsters", { monsters: monstersData });
+});
+
+app.get('/monsters/:slug', function(request, response) {
+
+	const foundMonster = monstersData.find( function(monster) {
+		return monster.slug == request.params.slug;
+	})
+	response.render("detail", { foundMonster });
 });
 
 app.get('/detail', function(request, response) {
@@ -33,6 +45,3 @@ app.use( function(request, response) {
 	response.status(404).render('404', { query: request.url });
 });
 
-app.listen(PORT, function() {
-	console.log("Server is listening at http://localhost:2000");
-});
